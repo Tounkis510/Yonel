8737c2954b7f42f77b0bf7ec1f1229931bfc594f
Tounkis510 <tounkisjunior@gmail.com>
2022-12-14 14:30:01 +0000

    msg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
