488e2dc3ceadb7818f1bd2b5b623176fb45c45cb
Tounkis510 <tounkisjunior@gmail.com>
2022-12-15 18:35:51 +0000

    msg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'app/service/user.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  readDataUser: any;
+  User: any;
+  showMsg: number;
+  mdp:any='tks'
+  login:any='BT'
 
-  constructor() { }
+  constructor(private service: UserService ) { }
 
   ngOnInit(): void {
+    this.service.getAllUsers().subscribe((res)=>{
+      console.log(res,'res==>');
+      this.readDataUser=res.data;
+      
+    });
   }
+  userForm = new FormGroup({
+    'login':new FormControl('',Validators.required),
+     'password':new FormControl('',Validators.required),
+   });
+ 
+   userSubmit(){  
+     
+     this.service.Connect(this.userForm.value).subscribe((res)=>{
+       console.log(res,'res==>');
+       var id=res.data[0].idUser;
+       if (res.data.length>0){
+       this.showMsg=1;}
+       else{
+       this.showMsg=2}
+       window.location.href="http://localhost:4200/#/home-agent";
+     });
+    }
 
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable,pipe,Subject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(public httpUser:HttpClient) { }
+  getAllUsers():Observable<any>{
+    return this.httpUser.get('http://127.0.0.1:3000/api/user/');
+  }
+  Connect(data:any):Observable<any>{//Authentification
+    return this.httpUser.post(`http://127.0.0.1:3000/connect`,data)
+    .pipe(map(user=>{
+  
+      localStorage.setItem("currentUser",JSON.stringify(user)); 
+      
+  
+      return user;
+      }));
+      
+  }
+}
